9d184e03cb6a1baa9448e14f0731cc8f3167aaed
Amir Arab <amir4rab@gmail.com>
2022-04-26 10:41:36 +0430

    added i18 support with next-translate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins');
+const nextTranslate = require('next-translate');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    nextTranslate,
+    withBundleAnalyzer
+  ], 
+  {
+    reactStrictMode: true,
+    experimental: {
+      outputStandalone: process.env.IS_DOCKER_BUILD === 'true' ? true : false,
+    },
+  }
+);
